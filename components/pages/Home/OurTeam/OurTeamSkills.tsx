55e08b5fda7210e736afe6cd257d852580a92873
alimat <alimatinfar1376@gmail.com>
2023-05-18 23:56:25 +0330

    fix bug in build

--- components/pages/Home/OurTeam/OurTeamSkills.tsx
+++ components/pages/Home/OurTeam/OurTeamSkills.tsx
@@ -8,21 +8,27 @@ type Props = {
 }
 
 function OurTeamSkills({skills}: Props) {
-  return skills.map((skill, index) => (
-    <DarkContainer key={index} className='flex flex-col space-y-4 h-64 md:h-full flex-1 aspect-square md:aspect-auto xl:aspect-square'>
-      <div className='relative w-14 md:w-20 h-14 md:h-20'>
-        <Image
-          fill
-          src={`/images/ourTeam/${skill.info.logo}`} alt={skill.info.logo}
-          style={{objectFit: 'contain'}}
-        />
-      </div>
+  return (
+    <>
+      {
+        skills.map((skill, index) => (
+          <DarkContainer key={index} className='flex flex-col space-y-4 h-64 md:h-full flex-1 aspect-square md:aspect-auto xl:aspect-square'>
+            <div className='relative w-14 md:w-20 h-14 md:h-20'>
+              <Image
+                fill
+                src={`/images/ourTeam/${skill.info.logo}`} alt={skill.info.logo}
+                style={{objectFit: 'contain'}}
+              />
+            </div>
 
-      <span className='text-white/70 font-light text-sm'>{skill.info.title}</span>
+            <span className='text-white/70 font-light text-sm'>{skill.info.title}</span>
 
-      <span>{skill.description}</span>
-    </DarkContainer>
-  ))
+            <span>{skill.description}</span>
+          </DarkContainer>
+        ))
+      }
+    </>
+  )
 }
 
 export default OurTeamSkills;
